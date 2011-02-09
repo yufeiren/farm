b49bd5b59105f2bea20148acd3e6783056c2ba5b
Ren Yufei <renyufei83@yahoo.com.cn>
2011-02-09 13:38:35 -0500

    add odirectread

--- Makefile
+++ Makefile
@@ -1,3 +1,10 @@
 CC=gcc
 CFLAGS=-g -O2
 
+all: odirectread
+
+%.o: %.c
+	$(CC) $(CFLAGS) $< -c
+	
+odirectread:
+	$(CC) $(CFLAGS) -o $@ $@.o
---
+++ odirectread.c
@@ -0,0 +1,44 @@
+#define _XOPEN_SOURCE 600
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#define __USE_GNU
+#include <fcntl.h>
+#include <string.h>
+
+int
+main(int argc, char **argv)
+{
+
+	void *buf;
+	int ret = 0;
+	
+	int ps = getpagesize();
+	printf("pagesize is: %d\n", ps);
+	
+	unsigned long long int bytes_written = 0;
+	int fd;
+	
+	if( (ret = posix_memalign(&buf, ps, ps*256)) != 0 ) {
+		perror("Memalign failed");
+		exit(ret);
+	}
+	
+	memset(buf, 0x00, ps*256);
+	
+	if( (fd = open(argv[1], O_RDONLY | O_DIRECT) ) < 0 ) {
+		perror("Open failed");
+		exit(ret);
+	}
+	
+	while (read(fd, buf, ps*256) > 0)
+		continue;
+	
+	printf("%s: finish read.", argv[0]);
+	close(fd);
+	free(buf);
+
+	
+}
