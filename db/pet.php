876c30e51d0171c8486c89b26cfc90f3c2356d94
Ren Yufei <renyufei83@yahoo.com.cn>
2011-03-28 20:05:54 -0400

    add login

---
+++ db/pet.php
@@ -0,0 +1,32 @@
+<?
+$username="root";
+$password="Cewit365";
+$database="menagerie";
+
+mysql_connect(localhost,$username,$password);
+@mysql_select_db($database) or die( "Unable to select database");
+$query="SELECT * FROM pet";
+$result=mysql_query($query);
+
+$num=mysql_numrows($result);
+
+mysql_close();
+
+echo "<b><center>Database Output</center></b><br><br>";
+
+$i=0;
+while ($i < $num) {
+
+$name=mysql_result($result,$i,"name");
+$owner=mysql_result($result,$i,"owner");
+$species=mysql_result($result,$i,"species");
+$sex=mysql_result($result,$i,"sex");
+$birth=mysql_result($result,$i,"birth");
+$death=mysql_result($result,$i,"death");
+
+echo "<b>$name $owner</b><br>Phone: $species<br>Mobile: $sex<br>Fax: $birth<br>E-mail: $death<br><hr><br>";
+
+$i++;
+}
+
+?>
