6c1c1c318adede6039c67affff7ef5a0fcf33b51
Ren Yufei <renyufei83@yahoo.com.cn>
2011-04-24 21:13:59 -0400

    debug parserss

--- xmlparse/parserss.c
+++ xmlparse/parserss.c
@@ -41,9 +41,9 @@ void walkTree(xmlNode * a_node)
   xmlAttr *cur_attr = NULL;
   for (cur_node = a_node; cur_node; cur_node = cur_node->next) {
      // do something with that node information, like… printing the tag’s name and attributes
-    printf(“Got tag : %s\n”, cur_node->name);
+    printf("Got tag : %s\n", cur_node->name);
     for (cur_attr = cur_node->properties; cur_attr; cur_attr = cur_attr->next) {
-      printf(“  -> with attribute : %s\n”, cur_attre->name);
+      printf("  -> with attribute : %s\n", cur_attre->name);
     }
     walkTree(cur_node->children);
   }
