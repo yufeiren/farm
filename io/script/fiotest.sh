c27714c0168f4b0c2349595f4cdbf1524de8ea11
Yufei Ren <renyufei83@gmail.com>
2011-05-21 20:31:14 -0400

    add fiotest.sh

--- io/script/fiotest.sh
+++ io/script/fiotest.sh
@@ -1,34 +1,34 @@
 #! /bin/bash
 
-Fio=/usr/bin/fio
+Fio=$(which fio)
+if [ ! -x $Fio ]; then
+	echo You need fio installed to start io evaluation
+	exit 1
+fi
+
 LocalDir=/data/fio
-LocalDiskFileIn=/data/fio/readhg
-LocalDiskFileOut=/data/fio/writehg
-FileoutSize=100G
+Testfile=fio-bench.dat
+FileoutSize=1000G
 Logdir=/home/ren/fiotest/fio/log
 Taskdir=/home/ren/fiotest/fio/task
+Runtime=600
 
 test -d $Logdir || mkdir -p $Logdir
 test -d $Taskdir || mkdir -p $Taskdir
 
-# ioengine: sync libaio
-# direct: 1 0
-# bs: 512K 1M 5M
-# rw: read randread write randwrite
 
-# read file
-for ioengine in libaio sync
+# read file test
+for rw in read write
 do
 	for direct in 0 1
 	do
-		for bs in 512K 1M 5M 10M
+		for bs in 8K 64K 512K 1M 5M 10M
 		do
-			for rw in read randread write randwrite
+			for ioengine in sync psync vsync libaio posixaio splice
 			do
 sleep 5;
-test -f $LocalDiskFileOut && rm -rf $LocalDiskFileOut
 sync; echo 3 > /proc/sys/vm/drop_caches
-script=$Taskdir/$ioengine-$direct-$bs-$rw
+script=$Taskdir/$rw-$direct-$bs-$ioengine
 touch $script
 echo "[global]" > $script
 echo "ioengine=$ioengine" >> $script
@@ -38,13 +38,13 @@ echo "rw=$rw" >> $script
 echo "directory=$LocalDir" >> $script
 echo "ioscheduler=deadline" >> $script
 echo "time_based" >> $script
-echo "runtime=60" >> $script
+echo "runtime=$Runtime" >> $script
 echo "" >> $script
 echo "[job]" >> $script
-echo "filename=readhg" >> $script
-echo "size=100g" >> $script
+echo "filename=$Testfile" >> $script
+echo "size=$FileoutSize" >> $script
 echo "iodepth=16" >> $script
-$Fio --bandwidth-log $script > $Logdir/$ioengine-$direct-$bs-$rw.log
+$Fio --bandwidth-log mylog --minimal $script
 			done
 		done
 	done
