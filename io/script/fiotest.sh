f9917f9723cc40c589564db870815ccb5914c375
Yufei Ren <renyufei83@gmail.com>
2011-05-21 21:05:00 -0400

    add fiotest.sh

--- io/script/fiotest.sh
+++ io/script/fiotest.sh
@@ -18,7 +18,7 @@ test -d $Taskdir || mkdir -p $Taskdir
 
 
 # read file test
-for rw in read write
+for rw in read
 do
 	for direct in 0 1
 	do
@@ -42,9 +42,41 @@ echo "runtime=$Runtime" >> $script
 echo "" >> $script
 echo "[job]" >> $script
 echo "filename=$Testfile" >> $script
-echo "size=$FileoutSize" >> $script
 echo "iodepth=16" >> $script
-$Fio --bandwidth-log mylog --minimal $script
+$Fio --minimal $script
+			done
+		done
+	done
+done
+
+# write file test
+for rw in write
+do
+	for direct in 0 1
+	do
+		for bs in 8K 64K 512K 1M 5M 10M
+		do
+			for ioengine in sync psync vsync libaio posixaio splice
+			do
+sleep 5;
+sync; echo 3 > /proc/sys/vm/drop_caches
+test -f $LocalDir/$Testfile && rm -rf $LocalDir/$Testfile
+script=$Taskdir/$rw-$direct-$bs-$ioengine
+touch $script
+echo "[global]" > $script
+echo "ioengine=$ioengine" >> $script
+echo "direct=$direct" >> $script
+echo "bs=$bs" >> $script
+echo "rw=$rw" >> $script
+echo "directory=$LocalDir" >> $script
+echo "ioscheduler=deadline" >> $script
+echo "time_based" >> $script
+echo "runtime=$Runtime" >> $script
+echo "" >> $script
+echo "[job]" >> $script
+echo "filename=$Testfile" >> $script
+echo "iodepth=16" >> $script
+$Fio --minimal $script
 			done
 		done
 	done
