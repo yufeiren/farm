d1d0795921c6d5481c2a11535ad26f19a166ae7c
Yufei Ren <renyufei83@gmail.com>
2011-05-29 18:02:00 -0400

    add webpage

--- kindleweb/web/index.php
+++ kindleweb/web/index.php
@@ -5,7 +5,7 @@ $database="kindleweb";
 
 mysql_connect(localhost,$username,$password);
 @mysql_select_db($database) or die( "Unable to select database");
-$query="SELECT * FROM kw_item_link";
+$query="SELECT * FROM kw_rss_link";
 $result=mysql_query($query);
 
 $num=mysql_numrows($result);
@@ -16,7 +16,6 @@ echo "<b><center>Database Output</center></b><br><br>";
 
 $i=0;
 while ($i < $num) {
-echo "<br>$i<br>";
 
 $title=mysql_result($result,$i,"title");
 $link=mysql_result($result,$i,"link");
