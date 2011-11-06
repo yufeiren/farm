b665907a12d126982ef57a303dba3a31810cced7
Yufei Ren <renyufei83@gmail.com>
2011-11-06 17:41:48 -0500

    update nersc script

--- script/nersc/rcftp-disk2mem-local.sh
+++ script/nersc/rcftp-disk2mem-local.sh
@@ -4,5 +4,12 @@ source /global/u2/s/shudong/sync/nersc/define.sh
 
 hn=`hostname`
 
-env RCFTPRC=$configdir/rcftprc-directio-1M-16 $rcftpbin -n -i -v < $taskdir/put-local > $logdir/rcftp-local-$hn.log
-
+if [ $hn = "cvrani04" ] || [ $hn = "cvrani08" ] || [ $hn = "cvrani12" ]; then
+env RCFTPRC=$configdir/rcftprc-directio-$bufsiz-$conn $rcftpbin -n -i -v < $taskdir/put-local-bc01 > $logdir/rcftp-local-$hn.log
+elif [ $hn = "cvrani05" ] || [ $hn = "cvrani09" ] || [ $hn = "cvrani13" ]; then
+env RCFTPRC=$configdir/rcftprc-directio-$bufsiz-$conn $rcftpbin -n -i -v < $taskdir/put-local-bc02 > $logdir/rcftp-local-$hn.log
+elif [ $hn = "cvrani06" ] || [ $hn = "cvrani10" ] || [ $hn = "cvrani14" ]; then
+env RCFTPRC=$configdir/rcftprc-directio-$bufsiz-$conn $rcftpbin -n -i -v < $taskdir/put-local-bc03 > $logdir/rcftp-local-$hn.log
+elif [ $hn = "cvrani07" ] || [ $hn = "cvrani11" ] || [ $hn = "cvrani15" ]; then
+env RCFTPRC=$configdir/rcftprc-directio-$bufsiz-$conn $rcftpbin -n -i -v < $taskdir/put-local-bc04 > $logdir/rcftp-local-$hn.log
+fi
