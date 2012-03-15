1f22f48fd47d92aaade641fd858005d8562060f6
Yufei Ren <renyufei83@gmail.com>
2012-03-15 17:57:57 -0400

    verilog tutorial

---
+++ verilog/one_day3.v
@@ -0,0 +1,6 @@
+case(address)
+  0 : $display ("It is 11:40PM");
+  1 : $display ("I am feeling sleepy");
+  2 : $display  ("Let me skip this tutorial");
+  default : $display  ("Need to complete");
+endcase
