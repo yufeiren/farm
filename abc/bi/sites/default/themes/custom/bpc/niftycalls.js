38282ce32fc9d397966cc29e6fb10077f4fa1978
Yufei Ren <renyufei83@gmail.com>
2012-03-25 21:21:07 -0400

    rm abc dir

--- abc/bi/sites/default/themes/custom/bpc/niftycalls.js
+++
@@ -1,6 +0,0 @@
-window.onload = function() {
-  Nifty("div#subnav,div#detailedContent,div#sidebar","none same-height");
-  Nifty("div#col1,div#col2","none same-height");
-  Nifty("div#col3,div#col4","none same-height");	
-  Nifty("div#boardMember-0,div#boardMember-1,div#boardMember-2,div#boardMember-3","none same-height");						   
-};
