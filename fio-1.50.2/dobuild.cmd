fbffdef4d822290262d10add552392f589832d98
Yufei Ren <renyufei83@gmail.com>
2012-03-31 21:29:54 -0400

    rm fio-1.50.2 log4u

--- fio-1.50.2/dobuild.cmd
+++
@@ -1,4 +0,0 @@
-"%WIX%\bin\candle" cygwin.wxs
-"%WIX%\bin\candle" install.wxs
-"%WIX%\bin\candle" examples.wxs
-"%WIX%\bin\light" install.wixobj cygwin.wixobj examples.wixobj -ext WixUIExtension -out fio.msi
