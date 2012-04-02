2bea7b39ced5762d95553a060de92f7b12342207
Yufei Ren <renyufei83@gmail.com>
2012-04-02 14:44:54 -0400

    update fio test script

--- fio/script/fio-rdma-server.sh
+++ fio/script/fio-rdma-server.sh
@@ -1,23 +1,18 @@
 #! /bin/bash
 
-Fio=/usr/local/bin/fio
+source ./defination
+
 if [ ! -x $Fio ]; then
 	echo You need fio installed to start io evaluation
 	exit 1
 fi
-
-DataSize=100g
-Logdir=/home/ren/fio/log
-Taskdir=/home/ren/fio/task
-
-ServIP=192.168.3.5
-ServPort=19001;
 
 rm -rf $Logdir
 rm -rf $Taskdir
 
 test -d $Logdir || mkdir -p $Logdir
 test -d $Taskdir || mkdir -p $Taskdir
+test -e $LogFile || touch $LogFile
 
 script=$Taskdir/rdma-server
 touch $script
@@ -28,9 +23,9 @@ echo "" >> $script
 # server
 for mode in RDMA_WRITE RDMA_READ SEND
 do
-	for bs in 512 1k 4k 32k 128k 512k 1m 4m 8m 16m 32m
+	for bs in $bss
 	do
-		for iodepth in 1 2 4 8 16 32 64
+		for iodepth in $iodepths
 		do
 # task
 job=[$mode-$bs-$iodepth]
@@ -38,7 +33,11 @@ echo $job >> $script
 #echo "thread" >> $script
 echo "filename="$ServIP/$ServPort >> $script
 echo "rw=read" >> $script
-echo "size="$DataSize >> $script
+if [ $bs = "512" || $bs = "1k" || $bs = "2k" ]; then
+	echo "size=20g" >> $script
+else
+	echo "size="$DataSize >> $script
+fi
 echo "bs="$bs >> $script
 echo "iodepth=1" >> $script
 echo "" >> $script
@@ -48,3 +47,4 @@ ServPort=$(($ServPort+1))
 done
 
 $Fio --minimal $script
+
