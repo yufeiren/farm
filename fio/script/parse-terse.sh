1e6569aa7629081ee5aa7268248977d6f6171ac4
Yufei Ren <renyufei83@gmail.com>
2012-04-06 10:37:42 -0400

    update parse terse for server side

--- fio/script/parse-terse.sh
+++ fio/script/parse-terse.sh
@@ -7,7 +7,15 @@ logfile=/home/ren/fio/log/rdma-test.log
 Tersedir=/home/ren/fio/terse
 test -d $Tersedir || mkdir -p $Tersedir
 
-# WRITE
+
+# cpu user - 88
+cut -f 88 -d ";" $logfile > $Tersedir/terse-cpuuser.log
+# cpu sys - 89
+cut -f 89 -d ";" $logfile > $Tersedir/terse-cpusys.log
+# context switches - 90
+cut -f 90 -d ";" $logfile > $Tersedir/terse-contextswitch.log
+
+# WRITE client side
 
 # IO(kb) - 47
 cut -f 47 -d ";" $logfile > $Tersedir/terse-write-totalio.log
@@ -21,10 +29,18 @@ cut -f 49 -d ";" $logfile > $Tersedir/terse-write-runtime.log
 # total latency: mean: 81 - microsecond
 cut -f 81 -d ";" $logfile > $Tersedir/terse-write-totallatencymean.log
 
-# cpu user - 88
-cut -f 88 -d ";" $logfile > $Tersedir/terse-write-cpuuser.log
-# cpu sys - 89
-cut -f 89 -d ";" $logfile > $Tersedir/terse-write-cpusys.log
-# context switches - 90
-cut -f 90 -d ";" $logfile > $Tersedir/terse-write-contextswitch.log
+
+# READ - server side
+
+# IO(kb) - 6
+cut -f 6 -d ";" $logfile > $Tersedir/terse-read-totalio.log
+
+# BW(kb) - 7
+cut -f 7 -d ";" $logfile > $Tersedir/terse-read-bandwidth.log
+
+# runtime(ms) - 9
+cut -f 9 -d ";" $logfile > $Tersedir/terse-read-runtime.log
+
+# total latency: mean: 40 - microsecond
+cut -f 40 -d ";" $logfile > $Tersedir/terse-read-totallatencymean.log
 
