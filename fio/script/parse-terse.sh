8586a3bf491371793c478bebabbc5819baaf2f58
Yufei Ren <renyufei83@gmail.com>
2012-04-15 16:57:22 -0400

    update BW field

--- fio/script/parse-terse.sh
+++ fio/script/parse-terse.sh
@@ -20,14 +20,14 @@ cut -f 90 -d ";" $logfile > $Tersedir/terse-contextswitch.log
 # IO(kb) - 47
 cut -f 47 -d ";" $logfile > $Tersedir/terse-write-totalio.log
 
-# BW(kb) - 48
-cut -f 48 -d ";" $logfile > $Tersedir/terse-write-bandwidth.log
-
 # runtime(ms) - 49
 cut -f 49 -d ";" $logfile > $Tersedir/terse-write-runtime.log
 
 # total latency: mean: 81 - microsecond
 cut -f 81 -d ";" $logfile > $Tersedir/terse-write-totallatencymean.log
+
+# BW(kb) mean - 86
+cut -f 86 -d ";" $logfile > $Tersedir/terse-write-bandwidth.log
 
 
 # READ - server side
@@ -35,12 +35,12 @@ cut -f 81 -d ";" $logfile > $Tersedir/terse-write-totallatencymean.log
 # IO(kb) - 6
 cut -f 6 -d ";" $logfile > $Tersedir/terse-read-totalio.log
 
-# BW(kb) - 7
-cut -f 7 -d ";" $logfile > $Tersedir/terse-read-bandwidth.log
-
 # runtime(ms) - 9
 cut -f 9 -d ";" $logfile > $Tersedir/terse-read-runtime.log
 
 # total latency: mean: 40 - microsecond
 cut -f 40 -d ";" $logfile > $Tersedir/terse-read-totallatencymean.log
+
+# BW(kb) mean - 45
+cut -f 45 -d ";" $logfile > $Tersedir/terse-read-bandwidth.log
 
