1bf72264241cdab837e2c4dccfdfecfdcf58e9ef
Yufei Ren <renyufei83@gmail.com>
2012-05-29 17:46:17 -0400

    remote class submission

--- class/ese501/tutorial/half_adder/half_adder.cpp
+++
@@ -1,6 +0,0 @@
-#include "half_adder.h"
-
-void half_adder::prc_half_adder() {
-	sum = a^b;
-	carry = a&b;
-}
--- class/ese588/vertical/example/c.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-x=0
-while [ $x -lt 10000 ]; do
-        printf "%d 1 2 3 4 5 6 7 8 9 10\n" $x
-        (( x++ ))
-done
