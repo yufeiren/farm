3da17bd88a3579f0151615438a5d3b98bdb68960
Yufei Ren <renyufei83@gmail.com>
2012-07-04 22:31:15 -0400

    update parse-server.sh

--- numa/iperf-tcp/parse-server.sh
+++ numa/iperf-tcp/parse-server.sh
@@ -6,11 +6,6 @@ test -e $Parsedir || mkdir -p $Parsedir
 
 # bandwidth
 
-for ps in $pss
-do
-	for bs in $bss
-	do
-
 for cpunode in $cpunodes
 do
 	for memnode in $memnodes
@@ -18,12 +13,8 @@ do
 
 # example output
 # [  4]  0.0-60.0 sec  63.3 GBytes  9.06 Gbits/sec
-tail -1 $Logdir/iperf-c$cpunode-m$memnode-$bs-$ps.log | cut -c 35-39 >> $Parsedir/iperf-$bs-$ps.log
+tail -1 $Logdir/iperf-server-c$cpunode-m$memnode.log | cut -c 35-39 >> $Parsedir/iperf-server-bw.log
 
 	done
 done
-
-	done
-done
-
 
