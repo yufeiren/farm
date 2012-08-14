9b0188369635f98fc1044c937fabf4ba03794b9c
Yufei Ren <renyufei83@gmail.com>
2012-08-14 16:30:41 -0400

    add nmonana project

---
+++ nmonana/nmon2db.sh
@@ -0,0 +1,43 @@
+#! /bin/bash
+
+fnmonlog=$1
+appname=$2
+
+apptoplog=$appname-top.log
+appzzzzorilog=$appname-ori-zzzz.log
+appzzzzlog=$appname-zzzz.log
+
+zzzzformat()
+{
+	while read line
+	do
+		id=`echo $line | cut -d ',' -f 1`
+				vtime=`echo $line | cut -d ',' -f 2`
+				vdate=`echo $line | cut -d ',' -f 3`
+				year=`echo $vdate | cut -d '-' -f 3`
+				mon=`echo $vdate | cut -d '-' -f 2 | sed -e "
+			s/JAN/01/
+			s/FEB/02/
+			s/MAR/03/
+			s/APR/04/
+			s/MAY/05/
+			s/JUN/06/
+			s/JUL/07/
+			s/AUG/08/
+			s/SEP/09/
+			s/OCT/10/
+			s/NOV/11/
+			s/DEC/12/"`
+				day=`echo $vdate | cut -d '-' -f 1`
+			printf "%s,%s-%s-%s %s\n" $id $year $mon $day $vtime >> $appzzzzlog
+	done < $appzzzzorilog
+}
+
+grep -e $appname $fnmonlog | grep "TOP" | cut -f 3,4 -d ',' > $appname-top.log
+
+grep -e "ZZZZ" $fnmonlog | cut -f 2,3,4 -d ',' > $appzzzzoriname
+
+zzzzformat
+
+cp $apptoplog example-top.log
+cp $appzzzzlog example-zzzz.log
---
+++ nmonana/nmonana.sql
@@ -0,0 +1,33 @@
+use nmon
+
+delete from nmon_zzzz;
+delete from nmon_top;
+delete from testcase;
+
+LOAD DATA LOCAL INFILE 'example-zzzz.log'
+INTO TABLE nmon.nmon_zzzz
+FIELDS TERMINATED BY ',' ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+(zzzz_tag, zzzz_date);
+
+LOAD DATA LOCAL INFILE 'example-top.log'
+INTO TABLE nmon.nmon_top
+FIELDS TERMINATED BY ',' ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+(top_tag, top_cpu);
+
+LOAD DATA LOCAL INFILE 'testcase-date-start-end.log'
+INTO TABLE nmon.testcase
+FIELDS TERMINATED BY ',' ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+(start, end);
+
+UPDATE nmon_top, nmon_zzzz
+ SET top_date = zzzz_date
+ WHERE top_tag = zzzz_tag;
+ 
+UPDATE testcase SET cpu = (select avg(nmon_top.top_cpu)
+ from nmon_top where testcase.start < nmon_top.top_date
+      and nmon_top.top_date < testcase.end );
+
+SELECT cpu FROM testcase ORDER BY start;
