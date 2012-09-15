179f0031c5462cbb50f485b6ae23ca5d039e6a42
Yufei Ren <renyufei83@gmail.com>
2012-09-15 18:05:42 -0400

    add hp iser evaluation

---
+++ paperexp/iser-eval-hp13.sh
@@ -0,0 +1,42 @@
+#! /bin/bash
+
+# start this script on Srv365-09
+
+# eval node0 sdd1 sde1 sdg1
+
+RunIOTests=/home/ren/git/farm/fio/run_io_tests.sh
+
+# unchanged
+expet="read randread write randwrite"
+bs="64 256 512 1024 4096 8192"
+ts="1 2 4 8 16"
+timeperiod=30
+
+# sync 1 file over ext4
+flist="/home/ren/iser/d0/f00"
+engine="sync"
+iodepths="1"
+
+numactl --cpunodebind=0 --membind=0 $RunIOTests -x $expet -f $flist -p -I -a fio -e $engine -b $bs -t $ts -i $iodepths -l $timeperiod -z 3 > iser-sync-f1.log 2>&1
+
+# libaio 1 file over ext4
+flist="/home/ren/iser/d0/f00"
+engine="libaio"
+iodepths="1 4 16 32"
+
+numactl --cpunodebind=0 --membind=0 $RunIOTests -x $expet -f $flist -p -I -a fio -e $engine -b $bs -t $ts -i $iodepths -l $timeperiod -z 3 > iser-libaio-f1.log 2>&1
+
+# sync 6 files over ext4
+flist="/home/ren/iser/d0/f00 /home/ren/iser/d0/f01 /home/ren/iser/d1/f00 /home/ren/iser/d1/f01 /home/ren/iser/d2/f00 /home/ren/iser/d2/f01"
+engine="sync"
+iodepths="1"
+
+numactl --cpunodebind=0 --membind=0 $RunIOTests -x $expet -f $flist -p -I -a fio -e $engine -b $bs -t $ts -i $iodepths -l $timeperiod -z 3 > iser-sync-f6.log 2>&1
+
+# libaio 6 file over ext4
+flist="/home/ren/iser/d0/f00 /home/ren/iser/d0/f01 /home/ren/iser/d1/f00 /home/ren/iser/d1/f01 /home/ren/iser/d2/f00 /home/ren/iser/d2/f01"
+engine="libaio"
+iodepths="1 4 16 32"
+
+numactl --cpunodebind=0 --membind=0 $RunIOTests -x $expet -f $flist -p -I -a fio -e $engine -b $bs -t $ts -i $iodepths -l $timeperiod -z 3 > iser-libaio-f6.log 2>&1
+
