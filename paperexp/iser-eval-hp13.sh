4f05412232bdd3cc926a8f9cf3cf51bb66688d23
Yufei Ren <renyufei83@gmail.com>
2012-09-15 18:22:36 -0400

    update logdir

--- paperexp/iser-eval-hp13.sh
+++ paperexp/iser-eval-hp13.sh
@@ -5,6 +5,7 @@
 # eval node0 sdd1 sde1 sdg1
 
 RunIOTests=/home/ren/git/farm/fio/run_io_tests.sh
+Logdir=~/ipdps13/
 
 # unchanged
 expet="read randread write randwrite"
@@ -17,26 +18,26 @@ flist="/home/ren/iser/d0/f00"
 engine="sync"
 iodepths="1"
 
-numactl --cpunodebind=0 --membind=0 $RunIOTests -x $expet -f $flist -p -I -a fio -e $engine -b $bs -t $ts -i $iodepths -l $timeperiod -z 3 > iser-sync-f1.log 2>&1
+numactl --cpunodebind=0 --membind=0 $RunIOTests -x $expet -f $flist -p -I -a fio -e $engine -b $bs -t $ts -i $iodepths -l $timeperiod -z 3 > $Logdir/iser-sync-f1.log 2>&1
 
 # libaio 1 file over ext4
 flist="/home/ren/iser/d0/f00"
 engine="libaio"
 iodepths="1 4 16 32"
 
-numactl --cpunodebind=0 --membind=0 $RunIOTests -x $expet -f $flist -p -I -a fio -e $engine -b $bs -t $ts -i $iodepths -l $timeperiod -z 3 > iser-libaio-f1.log 2>&1
+numactl --cpunodebind=0 --membind=0 $RunIOTests -x $expet -f $flist -p -I -a fio -e $engine -b $bs -t $ts -i $iodepths -l $timeperiod -z 3 > $Logdir/iser-libaio-f1.log 2>&1
 
 # sync 6 files over ext4
 flist="/home/ren/iser/d0/f00 /home/ren/iser/d0/f01 /home/ren/iser/d1/f00 /home/ren/iser/d1/f01 /home/ren/iser/d2/f00 /home/ren/iser/d2/f01"
 engine="sync"
 iodepths="1"
 
-numactl --cpunodebind=0 --membind=0 $RunIOTests -x $expet -f $flist -p -I -a fio -e $engine -b $bs -t $ts -i $iodepths -l $timeperiod -z 3 > iser-sync-f6.log 2>&1
+numactl --cpunodebind=0 --membind=0 $RunIOTests -x $expet -f $flist -p -I -a fio -e $engine -b $bs -t $ts -i $iodepths -l $timeperiod -z 3 > $Logdir/iser-sync-f6.log 2>&1
 
 # libaio 6 file over ext4
 flist="/home/ren/iser/d0/f00 /home/ren/iser/d0/f01 /home/ren/iser/d1/f00 /home/ren/iser/d1/f01 /home/ren/iser/d2/f00 /home/ren/iser/d2/f01"
 engine="libaio"
 iodepths="1 4 16 32"
 
-numactl --cpunodebind=0 --membind=0 $RunIOTests -x $expet -f $flist -p -I -a fio -e $engine -b $bs -t $ts -i $iodepths -l $timeperiod -z 3 > iser-libaio-f6.log 2>&1
+numactl --cpunodebind=0 --membind=0 $RunIOTests -x $expet -f $flist -p -I -a fio -e $engine -b $bs -t $ts -i $iodepths -l $timeperiod -z 3 > $Logdir/iser-libaio-f6.log 2>&1
 
