d4dd04fbaf4c4a9abb0a385b109aafd03e872646
Yufei Ren <renyufei83@gmail.com>
2012-09-19 15:49:48 -0400

    update iser eval script

--- paperexp/iser-eval-hp13.sh
+++ paperexp/iser-eval-hp13.sh
@@ -4,8 +4,10 @@
 
 # eval node0 sdd1 sde1 sdg1
 
+ts=auto
+
 RunIOTests=/home/ren/git/farm/fio/run_io_tests.sh
-Logdir=/home/ren/ipdps13/
+Logdir=/home/ren/ipdps13/$ts
 
 if [ ! -d $Logdir ]; then
 	mkdir -p $Logdir
@@ -15,33 +17,34 @@ fi
 expet="read randread write randwrite"
 bs="64 256 512 1024 4096 8192"
 ts="1 2 4 8 16"
-timeperiod=30
-
-# sync 1 raw device
-devlist="/dev/sdd"
-engine="sync"
-iodepths="1"
-
-numactl --cpunodebind=0 --membind=0 $RunIOTests -x "$expet" -d "$devlist" -p -I -a fio -e "$engine" -b "$bs" -t "$ts" -i "$iodepths" -l $timeperiod -z 3 -v -o $Logdir/iser-sync-f1.log > $Logdir/script.log 2>&1
-
-# libaio 1 raw device
-devlist="/dev/sdd"
-engine="libaio"
-iodepths="1 4 16 32"
-
-numactl --cpunodebind=0 --membind=0 $RunIOTests -x "$expet" -d "$devlist" -p -I -a fio -e "$engine" -b "$bs" -t "$ts" -i "$iodepths" -l $timeperiod -z 3 -v -o $Logdir/iser-libaio-f1.log > $Logdir/script.log 2>&1
+timeperiod=300
 
 # sync 6 files over ext4
 devlist="/dev/sdd /dev/sde /dev/sdg /dev/sdc /dev/sdf /dev/sdh"
 engine="sync"
 iodepths="1"
 
-$RunIOTests -x "$expet" -d "$devlist" -p -I -a fio -e "$engine" -b "$bs" -t "$ts" -i "$iodepths" -l $timeperiod -z 3 -v -o $Logdir/iser-sync-f6.log > $Logdir/script.log 2>&1
+# clean
+echo "" > $Logdir/$ts-date-start.log
+echo "" > $Logdir/$ts-date-end.log
+echo "" > $Logdir/$ts-sum.log
 
-# libaio 6 file over ext4
-devlist="/dev/sdd /dev/sde /dev/sdg /dev/sdc /dev/sdf /dev/sdh"
-engine="libaio"
-iodepths="1 4 16 32"
+for exp in $expet
+do
+	for b in $bs
+	do
+		for t in $ts
+		do
 
-$RunIOTests -x "$expet" -d "$devlist" -p -I -a fio -e "$engine" -b "$bs" -t "$ts" -i "$iodepths" -l $timeperiod -z 3 -v -o $Logdir/iser-libaio-f6.log > $Logdir/script.log 2>&1
+date "+%F %H:%M:%S" >> $Logdir/$ts-date-start.log
+
+$RunIOTests -x "$exp" -d "$devlist" -p -I -a fio -e "$engine" -b "$b" -t "$t" -i "$iodepths" -l $timeperiod -z 3 -v -o $Logdir/iser-sync-f6-$exp-b$b-t$t.log > $Logdir/script.log 2>&1
+
+tail -1 $Logdir/iser-sync-f6-$exp-b$b-t$t.log >> $Logdir/$ts-sum.log
+
+date "+%F %H:%M:%S" >> $Logdir/$ts-date-end.log
+
+		done
+	done
+done
 
