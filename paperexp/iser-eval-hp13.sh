6a77120032d2ff438c07663f5b39d8783c3653cf
Yufei Ren <renyufei83@gmail.com>
2012-09-19 16:00:28 -0400

    update

--- paperexp/iser-eval-hp13.sh
+++ paperexp/iser-eval-hp13.sh
@@ -4,10 +4,10 @@
 
 # eval node0 sdd1 sde1 sdg1
 
-ts=auto
+tc=auto
 
 RunIOTests=/home/ren/git/farm/fio/run_io_tests.sh
-Logdir=/home/ren/ipdps13/$ts
+Logdir=/home/ren/ipdps13/$tc
 
 if [ ! -d $Logdir ]; then
 	mkdir -p $Logdir
@@ -25,9 +25,9 @@ engine="sync"
 iodepths="1"
 
 # clean
-echo "" > $Logdir/$ts-date-start.log
-echo "" > $Logdir/$ts-date-end.log
-echo "" > $Logdir/$ts-sum.log
+echo "" > $Logdir/$tc-date-start.log
+echo "" > $Logdir/$tc-date-end.log
+echo "" > $Logdir/$tc-sum.log
 
 for exp in $expet
 do
@@ -36,13 +36,13 @@ do
 		for t in $ts
 		do
 
-date "+%F %H:%M:%S" >> $Logdir/$ts-date-start.log
+date "+%F %H:%M:%S" >> $Logdir/$tc-date-start.log
 
 $RunIOTests -x "$exp" -d "$devlist" -p -I -a fio -e "$engine" -b "$b" -t "$t" -i "$iodepths" -l $timeperiod -z 3 -v -o $Logdir/iser-sync-f6-$exp-b$b-t$t.log > $Logdir/script.log 2>&1
 
-tail -1 $Logdir/iser-sync-f6-$exp-b$b-t$t.log >> $Logdir/$ts-sum.log
+tail -1 $Logdir/iser-sync-f6-$exp-b$b-t$t.log >> $Logdir/$tc-sum.log
 
-date "+%F %H:%M:%S" >> $Logdir/$ts-date-end.log
+date "+%F %H:%M:%S" >> $Logdir/$tc-date-end.log
 
 		done
 	done
