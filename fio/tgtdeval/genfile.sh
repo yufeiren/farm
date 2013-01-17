e1a98ed0b568f8bdea964eaaa22dcf89eebfe119
Yufei Ren <renyufei83@gmail.com>
2013-01-17 15:40:45 -0500

    update genfile

--- fio/tgtdeval/genfile.sh
+++ fio/tgtdeval/genfile.sh
@@ -7,11 +7,11 @@ dir2=/mnt/sdb4
 
 while [ $i -lt 18 ];
 do
-	file1=/mnt/$dir1/f$i
-	file2=/mnt/$dir2/f$i
+	file1=$dir1/f$i
+	file2=$dir2/f$i
 	touch $file1
 	touch $file2
-	fio --size=$fsize --bs=512k --ioengine=libaio --iodepth=4 --rw=write --name=j0 --filename=$file1
-	fio --size=$fsize --bs=512k --ioengine=libaio --iodepth=4 --rw=write --name=j0 --filename=$file2
+	fio --direct=1 --size=$fsize --bs=512k --ioengine=libaio --iodepth=4 --rw=write --name=j0 --filename=$file1
+	fio --direct=1 --size=$fsize --bs=512k --ioengine=libaio --iodepth=4 --rw=write --name=j0 --filename=$file2
 done
 
