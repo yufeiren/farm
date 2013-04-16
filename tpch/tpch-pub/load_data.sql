adb414508c4141bc44d58e6e35097dffbfec486b
Yufei Ren <yufei.ren@stonybrook.edu>
2013-04-16 19:57:49 -0400

    update load data

--- tpch/tpch-pub/load_data.sql
+++ tpch/tpch-pub/load_data.sql
@@ -1,8 +1,8 @@
+load data infile '/data/tpch/region.tbl' into table region fields terminated by '|' ;
+load data infile '/data/tpch/nation.tbl' into table nation fields terminated by '|' ;
+load data infile '/data/tpch/part.tbl' into table part fields terminated by '|' ;
+load data infile '/data/tpch/supplier.tbl' into table supplier fields terminated by '|' ;
 load data infile '/data/tpch/partsupp.tbl' into table partsupp fields terminated by '|' ;
 load data infile '/data/tpch/customer.tbl' into table customer fields terminated by '|' ;
-load data infile '/data/tpch/nation.tbl' into table nation fields terminated by '|' ;
 load data infile '/data/tpch/orders.tbl' into table orders fields terminated by '|' ;
-load data infile '/data/tpch/region.tbl' into table region fields terminated by '|' ;
-load data infile '/data/tpch/supplier.tbl' into table supplier fields terminated by '|' ;
 load data infile '/data/tpch/lineitem.tbl' into table lineitem fields terminated by '|' ;
-load data infile '/data/tpch/part.tbl' into table part fields terminated by '|' ;
