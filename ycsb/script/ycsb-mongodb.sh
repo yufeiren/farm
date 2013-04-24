bd5b712184319f716f875963c0079f2fb6178316
Yufei Ren <renyufei83@gmail.com>
2013-04-23 21:07:14 -0400

    update onet testcases

--- ycsb/script/ycsb-mongodb.sh
+++ ycsb/script/ycsb-mongodb.sh
@@ -4,6 +4,8 @@ source ./defination
 
 test -d $Logdir || mkdir -p $Logdir
 
+$YCSBHOME/bin/ycsb load mongodb -P $Taskdir/wlonet-read100-up0 -s -threads 10 > $Logdir/wlonet-read100-up0.log
+
 for wl in $wls
 do
 	$YCSBHOME/bin/ycsb run mongodb -P $Taskdir/$wl -s -threads 10 > $Logdir/$wl.log
