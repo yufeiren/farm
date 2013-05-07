7856a845a2c6a91b8d3a5c3e76b4c1a879750555
Yufei Ren <yufei.ren@stonybrook.edu>
2013-05-06 20:47:39 -0400

    add query gen

---
+++ tpch/tpch-pub/genquery.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+Bindir=/home/ren/git/farm/tpch/tpch_2_15.0/dbgen
+Qgen=$Bindir/qgen
+
+scales="1 10 100 500 1000"
+
+cd $Bindir
+
+i=1
+
+for scale in $scales
+do
+	Targetdir=$Bindir/queries-mysql-s$scale
+
+	test -d $Targetdir || mkdir -p $Targetdir
+
+	while [ $i -le 22 ]; do
+		$Qgen -c -s $scale $i > $Targetdir/mysql-s$scale-q$i.sql
+		(( i++ ))
+	done
+done
+
