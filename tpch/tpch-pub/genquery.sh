7ee710c30d38367fabc9360733cd3b5c288a76f7
Yufei Ren <yufei.ren@stonybrook.edu>
2013-05-06 20:55:11 -0400

    update genquery.sh

--- tpch/tpch-pub/genquery.sh
+++ tpch/tpch-pub/genquery.sh
@@ -8,14 +8,13 @@ scales="1 10 100 500 1000"
 
 cd $Querydir
 
-i=1
-
 for scale in $scales
 do
 	Targetdir=$Bindir/queries-mysql-s$scale
 
 	test -d $Targetdir || mkdir -p $Targetdir
 
+	i=1
 	while [ $i -le 22 ]; do
 		$Qgen -c -s $scale $i > $Targetdir/mysql-s$scale-q$i.sql
 		(( i++ ))
