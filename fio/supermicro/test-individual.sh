95a850a97bf9f70f38d274bae08c9442c84ca50c
Yufei Ren <renyufei83@gmail.com>
2013-12-17 07:38:58 -0500

    update test script

--- fio/supermicro/test-individual.sh
+++ fio/supermicro/test-individual.sh
@@ -1,10 +1,13 @@
 #! /bin/bash
 
+logfile=p46disk.log
+echo > $logfile
+
 for ie in sync libaio
 do
 	for rw in read randread write randwrite
 	do
-		fio $ie-$rw.fio > p46disk.log
+		fio $ie-$rw.fio >> $logfile
 	done
 done
 
