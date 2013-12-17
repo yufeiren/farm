541657338c4395b01e9b5de42e375369a9fa60e6
Yufei Ren <renyufei83@gmail.com>
2013-12-17 09:48:25 -0500

    update

---
+++ fio/supermicro/format-disk-software-raid.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+for disk in b c d e f g h i j k l m n o p q r s t u v w x y z
+do
+	(echo o; echo n; echo p; echo 1; echo; echo; echo p; echo t; echo fd; echo p; echo w) | fdisk /dev/sd$disk
+done
+
+for disk in b c d e f g h i j k l m n o p q r s t u v
+do
+	(echo o; echo n; echo p; echo 1; echo; echo; echo p; echo t; echo fd; echo p; echo w) | fdisk /dev/sda$disk
+done
