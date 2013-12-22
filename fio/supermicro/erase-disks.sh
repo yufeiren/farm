ed8d28902ff5c23e862b08ccee03843e2882459a
Yufei Ren <renyufei83@gmail.com>
2013-12-21 21:19:51 -0500

    update disk err infomation and erase script

---
+++ fio/supermicro/erase-disks.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+for disk in b c d e f g h i j k l m n o p q r s t u v w x y z
+do
+	(echo d; echo p; echo w) | fdisk /dev/sd$disk
+done
+
+for disk in b c d e f g h i j k l m n o p q r s t u v
+do
+	(echo d; echo p; echo w) | fdisk /dev/sda$disk
+done
+
