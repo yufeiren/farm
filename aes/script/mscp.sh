3e422398e041ebbc6d7f5bd989a5074107202c39
Yufei Ren <renyufei83@gmail.com>
2014-01-01 15:53:42 -0500

    add scp script

---
+++ aes/script/mscp.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+# result is in ganglia
+# memdisk to /dev/null
+
+ms="1 2 4 8 16 32 64"
+cipher=aes128-cbc
+file=/mnt/tmp0/f60g
+period=600
+dest=192.168.1.9
+
+for m in $ms
+do
+	i=0
+	while [ $i -lt $m ]
+	do
+		scp -c $cipher $file ren@$dest:/dev/null &
+		sleep 1
+		(( i++ ))
+	done
+
+	sleep $period
+	killall -s 9 scp
+	sleep 10
+done
+
