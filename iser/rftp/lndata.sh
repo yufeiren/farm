73df7a857b83daa5a4de35a8a3fa87aada4edfcf
Yufei Ren <yufei.ren@stonybrook.edu>
2014-01-11 17:14:58 -0500

    update

--- iser/rftp/lndata.sh
+++ iser/rftp/lndata.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
 
 mkdir -p /home/ren/rftp/data/source-tmpfs/d
+mkdir -p /home/ren/rftp/data/source-tmpfs/d2
 mkdir -p /home/ren/rftp/data/sink-tmpfs/d
+mkdir -p /home/ren/rftp/data/sink-tmpfs/d2
 
 ln -s /home/ren/iser/d0/f00 /home/ren/rftp/data/source-tmpfs/d/d0-f00
 ln -s /home/ren/iser/d0/f01 /home/ren/rftp/data/source-tmpfs/d/d0-f01
@@ -10,9 +12,24 @@ ln -s /home/ren/iser/d1/f01 /home/ren/rftp/data/source-tmpfs/d/d1-f01
 ln -s /home/ren/iser/d2/f00 /home/ren/rftp/data/source-tmpfs/d/d2-f00
 ln -s /home/ren/iser/d2/f01 /home/ren/rftp/data/source-tmpfs/d/d2-f01
 
+ln -s /home/ren/iser/d3/f00 /home/ren/rftp/data/source-tmpfs/d2/d3-f00
+ln -s /home/ren/iser/d3/f01 /home/ren/rftp/data/source-tmpfs/d2/d3-f01
+ln -s /home/ren/iser/d4/f00 /home/ren/rftp/data/source-tmpfs/d2/d4-f00
+ln -s /home/ren/iser/d4/f01 /home/ren/rftp/data/source-tmpfs/d2/d4-f01
+ln -s /home/ren/iser/d5/f00 /home/ren/rftp/data/source-tmpfs/d2/d5-f00
+ln -s /home/ren/iser/d5/f01 /home/ren/rftp/data/source-tmpfs/d2/d5-f01
+
 ln -s /home/ren/iser/d0/f00 /home/ren/rftp/data/sink-tmpfs/d/d0-f00
 ln -s /home/ren/iser/d0/f01 /home/ren/rftp/data/sink-tmpfs/d/d0-f01
 ln -s /home/ren/iser/d1/f00 /home/ren/rftp/data/sink-tmpfs/d/d1-f00
 ln -s /home/ren/iser/d1/f01 /home/ren/rftp/data/sink-tmpfs/d/d1-f01
 ln -s /home/ren/iser/d2/f00 /home/ren/rftp/data/sink-tmpfs/d/d2-f00
 ln -s /home/ren/iser/d2/f01 /home/ren/rftp/data/sink-tmpfs/d/d2-f01
+
+ln -s /home/ren/iser/d3/f00 /home/ren/rftp/data/sink-tmpfs/d2/d3-f00
+ln -s /home/ren/iser/d3/f01 /home/ren/rftp/data/sink-tmpfs/d2/d3-f01
+ln -s /home/ren/iser/d4/f00 /home/ren/rftp/data/sink-tmpfs/d2/d4-f00
+ln -s /home/ren/iser/d4/f01 /home/ren/rftp/data/sink-tmpfs/d2/d4-f01
+ln -s /home/ren/iser/d5/f00 /home/ren/rftp/data/sink-tmpfs/d2/d5-f00
+ln -s /home/ren/iser/d5/f01 /home/ren/rftp/data/sink-tmpfs/d2/d5-f01
+
