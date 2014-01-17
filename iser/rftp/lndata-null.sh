811f6cac3b3efab432f8790ffa96c5d76d6d7308
Yufei Ren <renyufei83@gmail.com>
2014-01-16 23:42:37 -0500

    add put data to srv11

---
+++ iser/rftp/lndata-null.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+
+mkdir -p /home/ren/rftp/data/source-tmpfs/d
+mkdir -p /home/ren/rftp/data/source-tmpfs/d2
+mkdir -p /home/ren/rftp/data/sink-tmpfs/d
+mkdir -p /home/ren/rftp/data/sink-tmpfs/d2
+
+ln -s /dev/null /home/ren/rftp/data/source-tmpfs/d/d0-f00
+ln -s /dev/null /home/ren/rftp/data/source-tmpfs/d/d0-f01
+ln -s /dev/null /home/ren/rftp/data/source-tmpfs/d/d1-f00
+ln -s /dev/null /home/ren/rftp/data/source-tmpfs/d/d1-f01
+ln -s /dev/null /home/ren/rftp/data/source-tmpfs/d/d2-f00
+ln -s /dev/null /home/ren/rftp/data/source-tmpfs/d/d2-f01
+
+ln -s /dev/null /home/ren/rftp/data/source-tmpfs/d2/d3-f00
+ln -s /dev/null /home/ren/rftp/data/source-tmpfs/d2/d3-f01
+ln -s /dev/null /home/ren/rftp/data/source-tmpfs/d2/d4-f00
+ln -s /dev/null /home/ren/rftp/data/source-tmpfs/d2/d4-f01
+ln -s /dev/null /home/ren/rftp/data/source-tmpfs/d2/d5-f00
+ln -s /dev/null /home/ren/rftp/data/source-tmpfs/d2/d5-f01
+
+ln -s /dev/null /home/ren/rftp/data/sink-tmpfs/d/d0-f00
+ln -s /dev/null /home/ren/rftp/data/sink-tmpfs/d/d0-f01
+ln -s /dev/null /home/ren/rftp/data/sink-tmpfs/d/d1-f00
+ln -s /dev/null /home/ren/rftp/data/sink-tmpfs/d/d1-f01
+ln -s /dev/null /home/ren/rftp/data/sink-tmpfs/d/d2-f00
+ln -s /dev/null /home/ren/rftp/data/sink-tmpfs/d/d2-f01
+
+ln -s /dev/null /home/ren/rftp/data/sink-tmpfs/d2/d3-f00
+ln -s /dev/null /home/ren/rftp/data/sink-tmpfs/d2/d3-f01
+ln -s /dev/null /home/ren/rftp/data/sink-tmpfs/d2/d4-f00
+ln -s /dev/null /home/ren/rftp/data/sink-tmpfs/d2/d4-f01
+ln -s /dev/null /home/ren/rftp/data/sink-tmpfs/d2/d5-f00
+ln -s /dev/null /home/ren/rftp/data/sink-tmpfs/d2/d5-f01
+
