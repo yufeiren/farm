3cfcf4be1d054ec4a3787153800d3ea73ad11bbb
Yufei Ren <yufei.ren@stonybrook.edu>
2014-03-04 10:35:37 -0500

    add for mdtm

---
+++ iser/formdtm/tgtd-startup.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+# start tgtd process on specified numa node
+tgtd -C 1 --iscsi portal=*:13261 --iser port=13261
+
+tgtadm -C 1 --op update --mode sys --name State -v offline
+
+tgt-admin -C 1 -e -c ./targets.conf
+
+tgtadm -C 1 --op update --mode sys --name State -v ready
+
+# setup account and password
+tgtadm -C 1 --lld iser --mode account --op new --user "consumer" --password "Longsw0rd"
+
+tgtadm -C 1 --lld iser --mode account --op bind --tid 1 --user "consumer"
+
+tgtadm -C 1 --lld iser --mode target --op show
+
