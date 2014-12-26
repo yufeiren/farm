c7c0db0b7dad20d816b51643025966f458570075
Yufei Ren <yufei.ren@stonybrook.edu>
2014-12-26 11:23:53 -0500

    update

--- libevent/test.sh
+++ libevent/test.sh
@@ -5,8 +5,8 @@ export LD_LIBRARY_PATH=/usr/local/lib
 Bench=/home/ren/git/farm/libevent/bench
 Logdir=./
 
-Pipes=100 1000 10000 100000
-Backend=select poll epoll
+Pipes="100 1000 10000 100000"
+Backend="select poll epoll"
 
 for b in $Backend
 do
