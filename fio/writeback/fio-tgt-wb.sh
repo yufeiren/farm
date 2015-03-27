1ab5a3a8eb98161dde668f05f7427d74b8452e27
Yufei Ren <yufei.ren@stonybrook.edu>
2015-03-26 23:16:19 -0400

    add wb tests

---
+++ fio/writeback/fio-tgt-wb.sh
@@ -0,0 +1,45 @@
+#! /bin/bash
+
+# evaluate the tgt performance of 4 nodes NUMA host: srv365-11
+
+source ./defination
+
+if [ ! -x $Fio ]; then
+        echo You need fio installed to start io evaluation
+        exit 1
+fi
+
+rm -rf $Logdir
+rm -rf $Taskdir
+
+test -d $Logdir || mkdir -p $Logdir
+test -d $Taskdir || mkdir -p $Taskdir
+test -e $LogFile || touch $LogFile
+
+cat /dev/null > $Logdir/$TestSuite-thr.log
+cat /dev/null > $Logdir/$TestSuite-lat.log
+
+# throughput
+run_test() {
+for size in $sizes
+do
+	for rw in $rws
+	do
+		for bs in $bss
+		do
+ssh root@srv365-11.cewit.stonybrook.edu "echo 3 > /proc/sys/vm/drop_caches"
+sleep 3
+# throughput eval
+fio --minimal --rw=$rw --size=$size --ioengine=libaio --iodepth=8 --direct=1 --bs=$bs --name=w1 --filename=/dev/sdd --name=w2 --filename=/dev/sde --name=w3 --filename=/dev/sdf --name=w4 --filename=/dev/sdc >> $Logdir/$TestSuite-thr.log
+
+ssh root@srv365-11.cewit.stonybrook.edu "echo 3 > /proc/sys/vm/drop_caches"
+sleep 3
+# latency eval
+fio --minimal --rw=$rw --size=$size --ioengine=sync --iodepth=1 --direct=1 --bs=$bs --name=w1 --filename=/dev/sdd --name=w2 --filename=/dev/sde --name=w3 --filename=/dev/sdf --name=w4 --filename=/dev/sdc >> $Logdir/$TestSuite-lat.log
+		done
+	done
+done
+}
+
+run_test
+
